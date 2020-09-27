a17d2482dacd1a58d0f57f53aff0c2bc4a4d09a2
zuozoe <2804242794@qq.com>
2020-09-27 16:56:21 +0800

    update cancel text

--- android/src/main/java/com/yunpeng/alipay/AlipayModule.java
+++ android/src/main/java/com/yunpeng/alipay/AlipayModule.java
@@ -43,7 +43,10 @@ public class AlipayModule extends ReactContextBaseJavaModule{
                             // "8000"代表支付结果因为支付渠道原因或者系统原因还在等待支付结果确认，最终交易是否成功以服务端异步通知为准（小概率状态）
                             if (TextUtils.equals(resultStatus, "8000")) {
                                 Toast.makeText(getCurrentActivity(), "支付结果确认中", Toast.LENGTH_SHORT).show();
-
+                            }
+                            if(TextUtils.equals(resultStatus, "6001")){
+                                //用户取消
+                                Toast.makeText(getCurrentActivity(), "已取消支付", Toast.LENGTH_SHORT).show();
                             } else {
                                 // 其他值就可以判断为支付失败，包括用户主动取消支付，或者系统返回的错误
                                 Toast.makeText(getCurrentActivity(), "支付失败", Toast.LENGTH_SHORT).show();
